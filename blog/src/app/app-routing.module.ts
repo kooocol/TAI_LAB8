e1f89fc614c11ab9feb5b520b959b800bb359bfa
kooocol <nataliakocol27@gmail.com>
2019-04-05 18:06:26 +0200

    zadanie skończone

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
 import {BlogComponent} from "./components/blog/blog.component";
 import {QuizComponent} from "./components/quiz/quiz.component";
+import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
+import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
 
 const routes: Routes = [
   {
@@ -16,12 +18,16 @@ const routes: Routes = [
   },
   {
     path: 'blog',
-    component: BlogComponent,
+    component: BlogHomeComponent,
   },
   {
     path: 'quiz',
     component: QuizComponent,
 
+  },
+  {
+    path: 'blog/detail/:id',
+    component: BlogItemDetailsComponent
   }];
 
 @NgModule({
---
+++ blog/src/app/directives/text-format.directive.ts
@@ -0,0 +1,14 @@
+import {Directive, ElementRef, Host, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[TextFormat]'
+})
+export class TextFormatDirective {
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('blur') onBlur(){
+    const value = this.el.nativeElement.value;
+    this.el.nativeElement.value = value.toLowerCase();
+  }
+}
