948a4f154e6aa4209ad7284acf8e31b4cc5ee4be
kooocol <nataliakocol27@gmail.com>
2019-04-26 17:27:41 +0200

    SpringBoot

---
+++ pwsz/src/main/java/com/pwsz/PwszApplication.java
@@ -0,0 +1,13 @@
+package com.pwsz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwszApplication.class, args);
+	}
+
+}
---
+++ pwsz/src/main/java/com/pwsz/post/PostController.java
@@ -0,0 +1,31 @@
+package com.pwsz.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostController {
+
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService){
+        this.postService = postService;
+    }
+
+    @GetMapping("/api/posts")
+    public List<Post> getAllPosts() {
+        return postService.getAll();
+    }
+
+    @GetMapping("/api/posts/{id}")
+    public Post get(@PathVariable int id) {
+        return postService.get(id);
+    }
+}
